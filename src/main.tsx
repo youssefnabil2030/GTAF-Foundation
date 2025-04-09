6d77b87c9cc672c51b395828e6d95c8b4d928dc8
Youssef Nabil <146959536+youssefnabil2030@users.noreply.github.com>
2025-04-09 05:16:01 -0700

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+createRoot(doacument.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
